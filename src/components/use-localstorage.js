8bf63b4c77a4e4ef6451d918cb98c9e2c4da5375
Łukasz A. Pelc <lukasz.pelc@analizy.pl>
2022-03-25 11:40:34 +0100

    Use localStorage, change layout

---
+++ src/components/use-localstorage.js
@@ -0,0 +1,17 @@
+// zapis do/odczyt z LocalStorage
+
+import { useState } from "react"
+
+export const useLocalStorage = (name, defaultValue) => {
+    const [state, setState] = useState(() => {
+        return JSON.parse(window.localStorage.getItem(name)) || defaultValue;
+    });
+
+    const setValue = (value) => {
+        console.log('value:', value);
+        window.localStorage.setItem(name, JSON.stringify(value));
+        setState(value);
+    };
+
+    return [state, setValue];
+}
