0b1f7c26b0f91fec5feb19f02daeefbba5378020
Łukasz A. Pelc <lukasz.pelc.81@gmail.com>
2022-03-25 12:22:53 +0100

    Add form - autofocus text input, set error on input

--- src/components/item-add.js
+++ src/components/item-add.js
@@ -5,6 +5,7 @@ import { Button, TextField } from '@mui/material';
 function ItemAdd({handler}) {
     const [add, setAdd] = useState(false);
     const [text, setText] = useState('');
+    const [error, setError] = useState(false);
 
     const clickAdd = useCallback((ev) => {
         setAdd(true);
@@ -12,16 +13,23 @@ function ItemAdd({handler}) {
 
     const clickSend = useCallback((ev) => {
         ev.preventDefault();
-        text && handler(text);
+        if (! text) {
+            setError(true);
+            return;
+        }
+
+        handler(text);
         setText('');
         setAdd(false);
-    }, [text, setText, setAdd, handler]);
+        setError(false);
+    }, [text, setText, setAdd, setError, handler]);
 
     const clickCancel = useCallback((ev) => {
         ev.preventDefault();
         setAdd(false);
         setText('');
-    }, [setAdd, setText]);
+        setError(false);
+    }, [setAdd, setText, setError]);
 
     const onChangeField = useCallback(
         (ev) => {
@@ -34,7 +42,7 @@ function ItemAdd({handler}) {
             <Button variant="contained" onClick={clickAdd} sx={{minWidth: 32}}><i className='icon-plus'></i></Button>
             {add &&
                 <div>
-                    <TextField id='add-item' label='Wpisz zadanie' variant='outlined' margin='dense' onChange={onChangeField} value={text} />
+                    <TextField id='add-item' label='Wpisz zadanie' required={true} autoFocus={true} error={error} variant='outlined' margin='dense' onChange={onChangeField} value={text} />
                     <Button id='add-item-btn' className='btn' variant='contained' size='large' onClick={clickSend} sx={{ml: 2}}>Dodaj</Button>
                     <Button id='add-item-cancel' className='btn' variant='contained' size='large' onClick={clickCancel} sx={{ml: 2}}>Anuluj</Button>
                 </div>
