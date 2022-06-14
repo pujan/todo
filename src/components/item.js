0e1bfa6cd08128a15d6df1030692e14d1d18390a
Łukasz A. Pelc <lukasz.pelc@analizy.pl>
2022-06-14 23:18:14 +0200

    Fix layout

--- src/components/item.js
+++ src/components/item.js
@@ -4,14 +4,19 @@ import React from 'react'
 export default function Item({item, deleted, finished}) {
   return (
     <div className={'border border-zinc-400 rounded-lg p-5 my-2' + (! item.active ? ' finish' : '')}>
-      <Stack direction='row' justifyContent='space-between' alignItems='stretch'>
-        <div className={'text-item' + (! item.active ? ' line-through': '')}>{item.name}</div>
-        <div>
-        {item.active &&
-          <Button variant='contained' id={'finish-' + item.id} color='success' onClick={finished} sx={{mr: 1}}><i className='icon-ok'></i></Button>}
-          <Button variant='contained' id={'delete-' + item.id} color='error' onClick={deleted}><i className='icon-cancel'></i></Button>
-        </div>
-      </Stack>
+        <Stack
+          direction={{xs: 'column', md: 'row'}}
+          justifyContent='space-between'
+          alignItems='stretch'
+          spacing={{xs: 2, md: 0}}
+        >
+          <div className={'text-item' + (! item.active ? ' line-through': '')}>{item.name}</div>
+          <div>
+          {item.active &&
+            <Button variant='contained' id={'finish-' + item.id} color='success' onClick={finished} sx={{mr: 1}}><i className='icon-ok'></i></Button>}
+            <Button variant='contained' id={'delete-' + item.id} color='error' onClick={deleted}><i className='icon-cancel'></i></Button>
+          </div>
+        </Stack>
     </div>
   );
 }
