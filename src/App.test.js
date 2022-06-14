20820e72467738c6bf6ad950b26b3578393ad7a3
Łukasz A. Pelc <lukasz.pelc@analizy.pl>
2022-06-14 21:30:49 +0200

    Update to react 18.1, fix errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders My TODO header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/my todo/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Container } from '@mui/material';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Container sx={{px: 10, py: 5}}>
       <App />
     </Container>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
